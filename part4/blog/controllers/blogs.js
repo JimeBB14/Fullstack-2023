e3c816a17629b51910e3a5a0b4e728ca03ea57b9
JimeBB14 <145394078+JimeBB14@users.noreply.github.com>
2024-07-18 18:40:30 +0200

    4.17

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,76 +1,99 @@
-const blogsRouter = require('express').Router();
-const Blog = require('../models/blog');
-const mongoose = require('mongoose');
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
-// GET all blogs
 blogsRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({});
-  res.json(blogs);
-});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  res.json(blogs)
+})
 
-// POST a new blog
-blogsRouter.post('/', async (req, res) => {
-  const body = req.body;
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
+blogsRouter.post('/', async (request, response, next) => {
+  const body = request.body
+  const token = getTokenFrom(request)
+
+  if (!token) {
+    return response.status(401).json({ error: 'token missing' })
+  }
+
+  let decodedToken
+  try {
+    decodedToken = jwt.verify(token, process.env.SECRET)
+  } catch (error) {
+    return response.status(401).json({ error: 'token invalid or expired' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+  if (!user) {
+    return response.status(400).json({ error: 'invalid user' })
+  }
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
-  });
+    user: user._id
+  })
 
   try {
-    const savedBlog = await blog.save();
-    res.status(201).json(savedBlog);
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    response.status(201).json(savedBlog)
   } catch (exception) {
-    res.status(400).json({ error: exception.message });
+    next(exception)
   }
-});
+})
 
-// PUT to update a blog
 blogsRouter.put('/:id', async (req, res) => {
-  const body = req.body;
-
+  const body = req.body
   const blog = {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes,
-  };
+    likes: body.likes
+  }
 
   try {
-    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, { new: true });
-    res.json(updatedBlog);
+    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, { new: true })
+    res.json(updatedBlog)
   } catch (exception) {
-    res.status(400).json({ error: exception.message });
+    res.status(400).json({ error: exception.message })
   }
-});
+})
 
-// GET a specific blog by ID
+blogsRouter.delete('/:id', async (req, res) => {
+  try {
+    await Blog.findByIdAndDelete(req.params.id)
+    res.status(204).end()
+  } catch (exception) {
+    res.status(500).json({ error: 'Failed to delete blog' })
+  }
+})
+
 blogsRouter.get('/:id', async (req, res) => {
-    try {
-      const blog = await Blog.findById(req.params.id);
-      if (blog) {
-        res.json(blog);
-      } else {
-        res.status(404).end();
-      }
-    } catch (exception) {
-      if (exception.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' });
-      }
-      res.status(500).json({ error: 'Failed to fetch blog' });
+  try {
+    const blog = await Blog.findById(req.params.id)
+    if (blog) {
+      res.json(blog)
+    } else {
+      res.status(404).end()
     }
-  });
-  
-  // DELETE a blog
-  blogsRouter.delete('/:id', async (req, res) => {
-    try {
-      await Blog.findByIdAndDelete(req.params.id);
-      res.status(204).end();
-    } catch (exception) {
-      res.status(500).json({ error: 'Failed to delete blog' });
+  } catch (exception) {
+    if (exception.name === 'CastError') {
+      return res.status(400).send({ error: 'malformatted id' })
     }
-  });
-  
-  module.exports = blogsRouter;
+    res.status(500).json({ error: 'Failed to fetch blog' })
+  }
+})
+
+module.exports = blogsRouter
--- part4/blog/controllers/users.js
+++ part4/blog/controllers/users.js
@@ -1,6 +1,13 @@
-const bcrypt = require('bcrypt'); 
+const bcrypt = require('bcrypt');
 const usersRouter = require('express').Router();
 const User = require('../models/user');
+const Blog = require('../models/blog');
+
+usersRouter.get('/', async (req, res) => {
+    const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1, id: 1 })
+    res.json(users)
+  })
+  
 
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body;
