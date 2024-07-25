981b8cf3a0ac7412bd5bb5aca19e3779a40b5d4f
JimeBB14 <145394078+JimeBB14@users.noreply.github.com>
2024-07-25 12:46:36 +0300

    6.13

--- part6/redux-anecdotes-main/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes-main/src/components/AnecdoteList.jsx
@@ -1,18 +1,23 @@
+
 import { useDispatch, useSelector } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
+import { setNotification, clearNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
   const dispatch = useDispatch()
-  const anecdotes = useSelector(({ anecdotes, filter }) => {
-    return anecdotes
-      .filter(anecdote =>
-        anecdote.content.toLowerCase().includes(filter.toLowerCase())
-      )
-      .sort((a, b) => b.votes - a.votes)
+  const anecdotes = useSelector(state => {
+    const filteredAnecdotes = state.anecdotes.filter(anecdote =>
+      anecdote.content.toLowerCase().includes(state.filter.toLowerCase())
+    )
+    return [...filteredAnecdotes].sort((a, b) => b.votes - a.votes)
   })
 
   const vote = (id) => {
     dispatch(voteAnecdote(id))
+    dispatch(setNotification('You voted for an anecdote'))
+    setTimeout(() => {
+      dispatch(clearNotification())
+    }, 5000)
   }
 
   return (
