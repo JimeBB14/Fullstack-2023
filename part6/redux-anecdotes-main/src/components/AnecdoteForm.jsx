ff64062cbb2fb10bd7e493f4644de035e58d508e
JimeBB14 <145394078+JimeBB14@users.noreply.github.com>
2024-07-25 16:47:53 +0300

    6.15

--- part6/redux-anecdotes-main/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes-main/src/components/AnecdoteForm.jsx
@@ -1,19 +1,15 @@
+import React from 'react'
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
-import { setNotification, clearNotification } from '../reducers/notificationReducer'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     dispatch(createAnecdote(content))
-    dispatch(setNotification('You created a new anecdote'))
-    setTimeout(() => {
-      dispatch(clearNotification())
-    }, 5000)
   }
 
   return (
--- part6/redux-anecdotes-main/src/store.js
+++ part6/redux-anecdotes-main/src/store.js
@@ -1,4 +1,3 @@
-
 import { configureStore } from '@reduxjs/toolkit'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
